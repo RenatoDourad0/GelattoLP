febbacbf95985a3b7c0743bafdeab0c195517971
Renato Dourad0 <99191410+RenatoDourad0@users.noreply.github.com>
2022-10-16 13:55:32 -0300

    adiciona caminho absoluto para assets da pasta public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav';
 export default function Header() {
   return (
     <>
-      <img src="../../public/assets/icecream-full.png" alt="logo" />
+      <img src={`${process.env.PUBLIC_URL}/assets/gelatto-text-filled-blue-edge.svg`} alt="logo" />
       <Nav />
     </>
   )
